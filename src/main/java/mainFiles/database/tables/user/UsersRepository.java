45d5dd6fea0307898371246b242ae73e7a3c7ef0
RuslanName <ruslangeroy@gmail.com>
2025-01-25 01:34:42 +0300

    Update UsersRepository.java

--- src/main/java/mainFiles/database/tables/user/UsersRepository.java
+++ src/main/java/mainFiles/database/tables/user/UsersRepository.java
@@ -1,9 +1,11 @@
 package mainFiles.database.tables.user;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository("usersRepository")
 public interface UsersRepository extends CrudRepository<User, Long> {
+    @Query("SELECT count(u) > 0 FROM User u WHERE u.betboomId = :betboomId")
+    boolean existsByBetboomId(Long betboomId);
 }
-

df65047748401b892a386db516e4b51839606a61
RuslanName <ruslangeroy@gmail.com>
2025-01-26 00:55:09 +0300

    Delete src/main/java/mainFiles/database/tables directory

--- src/main/java/mainFiles/database/tables/question/QuestionsRepository.java
+++
@@ -1,9 +0,0 @@
-package mainFiles.database.tables.question;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("questionsRepository")
-public interface QuestionsRepository extends CrudRepository<Question, Integer> {
-}
-

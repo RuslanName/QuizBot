c59f2cb1aeaf7c640633818e82d539c98055b32d
RuslanName <ruslangeroy@gmail.com>
2025-01-28 18:45:16 +0300

    Delete src/main/java directory

--- src/main/java/mainFiles/database/tables/differentState/DifferentStatesRepository.java
+++
@@ -1,9 +0,0 @@
-package mainFiles.database.tables.differentState;
-
-import mainFiles.database.utils.customRepositoryMethods.CustomRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("differentStatesRepository")
-public interface DifferentStatesRepository extends CrudRepository<DifferentState, Long>, CustomRepository<DifferentState> {
-}
